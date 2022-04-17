8cc46c7ac344eede9c9d61cec65e1e114e58e16c
malizkl <63977569+malizkl@users.noreply.github.com>
2022-04-17 22:30:34 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Mail
+
+go 1.15
+
+require github.com/aws/aws-sdk-go v1.43.41 // indirect
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+)
+
+const (
+	Sender    = "alizkl.cs@gmail.com"
+	Recipient = "alizkl.cs@gmail.com"
+	Subject   = "Hello Mail"
+	HtmlBody  = "<h1 style='color:blue'>An Email</h1>"
+	TextBody  = "This is an Classic Email TExt Body"
+	CharSet   = "UTF-8"
+)
+
+func main() {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Profile: "Ali",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	svc := ses.New(sess)
+
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(Recipient),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(HtmlBody),
+				},
+				Text: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(TextBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(Subject),
+			},
+		},
+		Source: aws.String(Sender),
+	}
+	result, err := svc.SendEmail(input)
+
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	fmt.Println("Email sent to address:" + Recipient)
+	fmt.Println(result)
+}
